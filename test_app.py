47045c09f16086aeef22a0da46b39633bc3d3a15
gehrimor <moritz.gehrig@gmail.com>
2025-02-02 20:23:52 +0100

    try linter fix

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
-import pytest
 from app import app
+
 
 def test_home():
     client = app.test_client()
@@ -7,8 +7,10 @@ def test_home():
     assert response.status_code == 200
     assert response.get_json() == {'message': 'Hello, World!'}
 
+
 def test_add():
     client = app.test_client()
     response = client.get('/add/2/3')
     assert response.status_code == 200
     assert response.get_json() == {'result': 5}
+
